899eef1d7cf62edcfe5418832755daf2c5ef942c
daewon-heo <eo7655@naver.com>
2018-10-04 22:36:01 +0900

    소스정리

--- web_Front_end/4.Jquery/수업소스/web/resource/js/rainbowColor.js
+++ web_Front_end/4.Jquery/수업소스/web/resource/js/rainbowColor.js
@@ -1,33 +1,31 @@
-$('input:checkbox[name^=colorAll]').change(function(){
-    if($(this).prop("checked")){
-        $(this).parent().attr("class", $(this).val());
-        $(this).siblings("input").prop("checked",false);
-        $(this).parents("tr").siblings().children("td[id^=row]").children("input[value=" +$(this).val() + "]").parent().attr("class", $(this).val());
-        $(this).parents("tr").siblings().children("td[id^=row]").children("input[value=" +$(this).val() + "]").prop("checked", true);
-        $(this).parents("tr").siblings().children("td[id^=row]").children("input[value!=" +$(this).val() + "]").prop("checked", false);
-    }else{
-        $(this).parents("tr").siblings().children("td[id^=row]").children("input[value=" +$(this).val() + "]").prop("checked", false);
-        $(this).parents("tr").siblings().children("td[id^=row]").removeClass($(this).val());
-        $(this).parents("td").removeClass($(this).val());
+$('input:checkbox[name^=color]').click(function(){
+    if($(this).prop("checked")){                                        // 전체 체크했을때
+        $("td[id^=row]").attr("class", $(this).val());                      // td 색깔 클래스 추가
+        $("input[value=" + $(this).val() + "]").prop("checked", true);      // 같은값 input 체크
+        $("input[value!=" + $(this).val() + "]").prop("checked", false);    // 다른값 input 체크 해제
+    }else{                                                              // 전체 체크 풀었을때
+        $("input[value=" + $(this).val() + "]").prop("checked", false);     // 같은값 input 체크 해제
+        $("td[id^=row]").removeClass();                                     // td 색깔 클래스 제거
     }
 });
 
-$('input:checkbox[name^=rainbow]').change(function(){
-    $(this).parent().attr("class", $(this).val());
-    $(this).siblings("input").prop("checked",false);
-
-    if($("input:checkbox[name^=rain][value=" + $(this).val() + "]:checked").length == 7){
-        $("#row0").attr("class",$(this).val());
-        $("#row0>input[value=" + $(this).val()+ "]").prop("checked", true);
-    }else{
-        $("#row0>input").prop("checked", false);
-        $("#row0").removeClass();
+$('input:checkbox[name^=rain]').click(function(){
+    if($(this).prop("checked")) {                                                         // 일반 체크했을때
+        $(this).parent().attr("class", $(this).val());                                        // td 색깔 클래스 추가
+        $(this).siblings().prop("checked",false);                                             // 나머지 input 체크 해제
+    }else                                                                                 // 일반 체크 풀었을때
+        $(this).parent().removeClass();                                                      // td 색깔 클래스 제거
+    
+    if($("input:checkbox[name^=rain][value=" + $(this).val() + "]:checked").length == 7){ // 일반 & 같은값 input이 7개일때 
+        $("#row0").attr("class", $(this).val());                                              // 전체 td 색깔 클래스 추가
+        $("#row0>input[value=" + $(this).val() + "]").prop("checked", true);                  // 전체 input 체크
+    }else{                                                                                // 일반 & 같은값 input이 7개가 아닐떄
+        $("#row0>input").prop("checked", false);                                              // 전체 input 체크 해제
+        $("#row0").removeClass();                                                             // 전체 td 클래스 제거
     }
-
-    if(!$(this).prop("checked")) $(this).parents("td").removeClass($(this).val());
 });
 
-$("#resetFrm").click(function(){
-    $("td").removeClass();
-    $("input").prop("checked", false);
+$("#resetFrm").click(function(){        // 리셋클릭시
+    $("td").removeClass();                  // 전체 td 클래스 제거
+    $("input").prop("checked", false);      // 전체 input 체크 해제
 });
